c54a68b5230384b340438d8df31f60683214f5c1
Mojahidul <Mojahidul@DESKTOP-EEV15GK>
2022-05-06 01:03:57 +0600

    Calculate Using String Operation-new class

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -1,68 +1,10 @@
-﻿using System;
-
-namespace ProblemSolving
+﻿namespace ProblemSolving
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("First Number: ");
-            float one = Convert.ToSingle(Console.ReadLine());
-
-            Console.Write("Operator: ");
-            string op = Console.ReadLine();
-
-            Console.Write("Last Number: ");
-            float two = Convert.ToSingle(Console.ReadLine());
-
-            // If Statement
-            if (op == "+")
-            {
-                Console.WriteLine("Result: " + (one + two));
-            }
-            else if (op == "-")
-            {
-                Console.WriteLine("Result: " + (one - two));
-            }
-            else if (op == "*")
-            {
-                Console.WriteLine("Result: " + (one * two));
-            }
-            else if(op == "/")
-            {
-                Console.WriteLine("Result: " + (one / two));
-            }
-            else if(op == "%")
-            {
-                Console.WriteLine("Result: " + (one % two));
-            }
-            else
-            {
-                Console.WriteLine("error!");
-            }
-
-            // Switch Case
-            switch (op)
-            {
-                case "+":
-                    Console.WriteLine("Result: " + (one + two));
-                    break;
-                case "-":
-                    Console.WriteLine("Result: " + (one - two));
-                    break;
-                case "*":
-                    Console.WriteLine("Result: " + (one * two));
-                    break;
-                case "/":
-                    Console.WriteLine("Result: " + (one / two));
-                    break;
-                case "%":
-                    Console.WriteLine("Result: " + (one % two));
-                    break;
-                default:
-                    Console.WriteLine("Result: Invalid!");
-                    break;
-            }
+            
         }
     }
 }
