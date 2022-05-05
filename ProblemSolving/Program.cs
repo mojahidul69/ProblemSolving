3f933a80677f8b440f83352ef0d259f3dce01d39
Mojahidul <Mojahidul@DESKTOP-EEV15GK>
2022-05-06 00:46:14 +0600

    Calculate Using String Operation

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -6,7 +6,63 @@ namespace ProblemSolving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("First Number: ");
+            float one = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Last Number: ");
+            float two = Convert.ToSingle(Console.ReadLine());
+
+            // If Statement
+            if (op == "+")
+            {
+                Console.WriteLine("Result: " + (one + two));
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine("Result: " + (one - two));
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine("Result: " + (one * two));
+            }
+            else if(op == "/")
+            {
+                Console.WriteLine("Result: " + (one / two));
+            }
+            else if(op == "%")
+            {
+                Console.WriteLine("Result: " + (one % two));
+            }
+            else
+            {
+                Console.WriteLine("error!");
+            }
+
+            // Switch Case
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine("Result: " + (one + two));
+                    break;
+                case "-":
+                    Console.WriteLine("Result: " + (one - two));
+                    break;
+                case "*":
+                    Console.WriteLine("Result: " + (one * two));
+                    break;
+                case "/":
+                    Console.WriteLine("Result: " + (one / two));
+                    break;
+                case "%":
+                    Console.WriteLine("Result: " + (one % two));
+                    break;
+                default:
+                    Console.WriteLine("Result: Invalid!");
+                    break;
+            }
         }
     }
 }
