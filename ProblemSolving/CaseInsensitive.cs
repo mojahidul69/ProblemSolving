664cf8535b9d59ff8775efad3b62d3ae201b3f9e
Mojahidul <Mojahidul@DESKTOP-EEV15GK>
2022-05-30 00:33:02 +0600

    CaseInsensitive

---
+++ ProblemSolving/CaseInsensitive.cs
@@ -0,0 +1,25 @@
+﻿namespace ProblemSolving
+{
+    public class CaseInsensitive
+    {
+        public string str1;
+        public string str2;
+        public bool CaseInsen()
+        {
+            bool flag = false;
+            if (str1.ToUpper() == str2.ToUpper())
+            {
+                flag = true;
+            }
+            return flag;
+        }
+        /*CaseInsensitive caseInsensitive = new CaseInsensitive();
+
+        string str1 = "hello";
+        string str2 = "hEllo";
+        caseInsensitive.str1 = str1;
+        caseInsensitive.str2 = str2;
+
+        Console.WriteLine(caseInsensitive.CaseInsen());*/
+    }
+}
--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -5,53 +5,17 @@ namespace ProblemSolving
     {
         static void Main(string[] args)
         {
-            AmazingReplaceNotAmazing.input = "Edabit is amazing";
-            string outP = AmazingReplaceNotAmazing.Amazing();
-            Console.WriteLine(outP);
-            /*string input = "Edabit is amazing";
-            string[] words = input.Split(' ');
+            string str1 = "hello";
+            string str2 = "hEllo";
 
-            string result = "";
-
-            foreach (string str in words)
+            if (str1.ToUpper() == str2.ToUpper())
             {
-                if (str == "Edabit")
-                {
-                    result = input;
-                    break;
-                }
-                else
-                {
-                    result = input.Replace("amazing", "not amazing");
-                }
+                Console.WriteLine(true);
             }
-            Console.WriteLine(result);*/
-
-
-            /*string input = "EdabitA is amazing";
-            string[] words = input.Split(" ");
-            string result = "";
-
-            for (int i = 0; i < words.Length; i++)
+            else
             {
-                if (words[i] == "Edabit")
-                {
-                    result = input;
-                    break;
-                    //Console.WriteLine(input);
-                }
-                else
-                {
-                    if (words[i] == "amazing")
-                    {
-                        words[i] = "not amazing";
-                    }
-                    result += words[i] + " ";
-                    //result = input.Replace("amazing", "not amazing");
-                    //Console.WriteLine(input.Replace("amazing", "not amazing"));
-                }
+                Console.WriteLine(false);
             }
-            Console.WriteLine(result);*/
         }
     }
 }
